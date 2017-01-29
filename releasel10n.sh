4a9560216e4557434241e8cab75e710a7578f54d
Buschmann <kontakt@buschmann23.de>
2017-01-29 11:31:06 +0100

    Add Spanish (Latin America) es_419 translation.

--- releasel10n.sh
+++ releasel10n.sh
@@ -2,7 +2,7 @@
 
 lrelease-qt5 -idbased translations/btsc.ts
 
-for LANG in da de sv
+for LANG in da de es_419 sv
 do
 lrelease-qt5 -idbased translations/btsc_$LANG.ts
 done
---
+++ translations/btsc_es_419.ts
@@ -0,0 +1,250 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="es_419" sourcelanguage="en" version="2.1">
+<context>
+    <name/>
+    <message id="btsc-homepage">
+        <location filename="../qml/AboutPage.qml" line="+90"/>
+        <source>Homepage</source>
+        <translation>Inicio</translation>
+    </message>
+    <message id="btsc-priv-policy">
+        <location line="+6"/>
+        <source>Privacy Policy</source>
+        <translation>Política de Privacidad</translation>
+    </message>
+    <message id="btsc-changelog">
+        <location line="+6"/>
+        <location filename="../qml/Changelog.qml" line="+41"/>
+        <source>Changelog</source>
+        <translation>Historial de los Cambios</translation>
+    </message>
+    <message id="btsc-contributors">
+        <location line="+6"/>
+        <location filename="../qml/Contributors.qml" line="+47"/>
+        <source>Contributors</source>
+        <translation>Contribuyentes</translation>
+    </message>
+    <message id="btsc-about">
+        <location line="+14"/>
+        <source>About</source>
+        <translation>Sobre Nosotros</translation>
+    </message>
+    <message id="btsc-contact">
+        <location line="+78"/>
+        <source>Contact</source>
+        <translation>Contacto</translation>
+    </message>
+    <message id="btsc-contribute">
+        <location line="+87"/>
+        <source>Contribute</source>
+        <translation>Contribuye</translation>
+    </message>
+    <message id="btsc-translate">
+        <location line="+12"/>
+        <source>Translate</source>
+        <translation>Traduce</translation>
+    </message>
+    <message id="btsc-report-bugs">
+        <location line="+8"/>
+        <source>Report bugs</source>
+        <translation>Reporta errores</translation>
+    </message>
+    <message id="btsc-donate">
+        <location line="+8"/>
+        <location filename="../qml/PaypalChooser.qml" line="+216"/>
+        <source>Donate</source>
+        <translation>Dona</translation>
+    </message>
+    <message id="btsc-third-party">
+        <location line="+22"/>
+        <source>3rd party components</source>
+        <translation>Componentes de 3ros</translation>
+    </message>
+    <message id="btsc-third-party-desc">
+        <location line="+9"/>
+        <source>%1 uses the following components:</source>
+        <oldsource>%1 uses the following third party components.</oldsource>
+        <translation>1% usa los siguientes componentes:</translation>
+    </message>
+    <message id="btsc-new">
+        <location filename="../qml/Changelog.qml" line="+22"/>
+        <source>New</source>
+        <translation>Nuevo</translation>
+    </message>
+    <message id="btsc-improved">
+        <location line="+20"/>
+        <source>Improved</source>
+        <translation>Mejorado</translation>
+    </message>
+    <message id="btsc-fixed">
+        <location line="+20"/>
+        <source>Fixed</source>
+        <translation>Arreglado</translation>
+    </message>
+    <message id="btsc-note">
+        <location line="+20"/>
+        <source>Note</source>
+        <translation>Nota</translation>
+    </message>
+    <message id="btsc-website">
+        <location filename="../qml/ContributorsDelegate.qml" line="+140"/>
+        <source>Website</source>
+        <translation>Sitio Web</translation>
+    </message>
+    <message id="btsc-currency">
+        <location filename="../qml/PaypalChooser.qml" line="-54"/>
+        <source>Currency</source>
+        <translation>Moneda de Cambio</translation>
+    </message>
+    <message id="btsc-eur">
+        <location line="+2"/>
+        <source>Euro</source>
+        <translation>Euro</translation>
+    </message>
+    <message id="btsc-usd">
+        <location line="+2"/>
+        <source>US Dollar</source>
+        <translation>Dólar Americano</translation>
+    </message>
+    <message id="btsc-aud">
+        <location line="+2"/>
+        <source>Australian Dollar</source>
+        <translation>Dólar Australiano</translation>
+    </message>
+    <message id="btsc-brl">
+        <location line="+2"/>
+        <source>Brazilian Real</source>
+        <translation>Real Brasilero</translation>
+    </message>
+    <message id="btsc-gbp">
+        <location line="+2"/>
+        <source>Pound Sterling</source>
+        <translation>Libra Esterlina</translation>
+    </message>
+    <message id="btsc-dkk">
+        <location line="+2"/>
+        <source>Danish Krone</source>
+        <translation>Corona Danesa</translation>
+    </message>
+    <message id="btsc-hkd">
+        <location line="+2"/>
+        <source>Hong Kong Dollar</source>
+        <translation>Dólar de Hong Kong</translation>
+    </message>
+    <message id="btsc-ils">
+        <location line="+2"/>
+        <source>Israel New Shekel</source>
+        <translation>Nuevo Shekel de Israel</translation>
+    </message>
+    <message id="btsc-jpy">
+        <location line="+2"/>
+        <source>Japanese Yen</source>
+        <translation>Yen Japonés</translation>
+    </message>
+    <message id="btsc-cad">
+        <location line="+2"/>
+        <source>Canadian Dollar</source>
+        <translation>Dólar Canadiense</translation>
+    </message>
+    <message id="btsc-mxn">
+        <location line="+2"/>
+        <source>Mexican Peso</source>
+        <translation>Peso Mexicano</translation>
+    </message>
+    <message id="btsc-twd">
+        <location line="+2"/>
+        <source>New Taiwan Dollar</source>
+        <translation>Nuevo Dólar Taiwanés</translation>
+    </message>
+    <message id="btsc-nzd">
+        <location line="+2"/>
+        <source>New Zealand Dollar</source>
+        <translation>Dólar Nueva Zelanda</translation>
+    </message>
+    <message id="btsc-nok">
+        <location line="+2"/>
+        <source>Norwegian Krone</source>
+        <translation>Corona de Noruega</translation>
+    </message>
+    <message id="btsc-php">
+        <location line="+2"/>
+        <source>Philippine Peso</source>
+        <translation>Peso Filipino</translation>
+    </message>
+    <message id="btsc-pln">
+        <location line="+2"/>
+        <source>Polish Zloty</source>
+        <translation>Zloty Polaco</translation>
+    </message>
+    <message id="btsc-rub">
+        <location line="+2"/>
+        <source>Russian Ruble</source>
+        <translation>Rublo Ruso</translation>
+    </message>
+    <message id="btsc-sek">
+        <location line="+2"/>
+        <source>Swedish Krone</source>
+        <translation>Corona Sueca</translation>
+    </message>
+    <message id="btsc-chf">
+        <location line="+2"/>
+        <source>Swiss Franc</source>
+        <translation>Franco Suizo</translation>
+    </message>
+    <message id="btsc-sgd">
+        <location line="+2"/>
+        <source>Singapore Dollar</source>
+        <translation>Dólar Singapur</translation>
+    </message>
+    <message id="btsc-thb">
+        <location line="+2"/>
+        <source>Thai Baht</source>
+        <translation>Baht Tailandés</translation>
+    </message>
+    <message id="btsc-czk">
+        <location line="+2"/>
+        <source>Czech Koruna</source>
+        <translation>Corona Checa</translation>
+    </message>
+    <message id="btsc-huf">
+        <location line="+2"/>
+        <source>Hungarian Forint</source>
+        <translation>HUF Forint Húngaro</translation>
+    </message>
+    <message id="btsec-lic-item-text">
+        <location filename="../qml/LicenseDelegate.qml" line="+60"/>
+        <source>%1 %2 by %3</source>
+        <oldsource>%1 %2 by %3 is licensed under %4.</oldsource>
+        <extracomment>%1 is the name of the sofware, %2 is the version, %3 is the author's name</extracomment>
+        <translation>%1 %2 por %3</translation>
+    </message>
+    <message id="btsec-lic-item-without-version-text">
+        <location line="+3"/>
+        <source>%1 by %2.</source>
+        <oldsource>%1 by %2 is licensed under %3.</oldsource>
+        <extracomment>%1 is the name of the sofware, %2 is the author's name</extracomment>
+        <translation>%1 por %2</translation>
+    </message>
+    <message id="btsc-license-web">
+        <location filename="../qml/licenses/LicenseBase.qml" line="+56"/>
+        <source>License website</source>
+        <translation>Licencia del Sitio Web</translation>
+    </message>
+    <message id="btsc-comp-web">
+        <location line="+7"/>
+        <source>Visit %1</source>
+        <extracomment>%1 is the name of the 3rd party component</extracomment>
+        <translation>Visita %1</translation>
+    </message>
+    <message id="btsc-back-to-top">
+        <location line="+8"/>
+        <source>Back to top</source>
+        <translation>De regreso hacia arriba</translation>
+    </message>
+    <message id="btsc-license-intro">
+        <location line="+33"/>
+        <source>%1 is available under the terms of the following license:</source>
+        <extracomment>%1 is the name of the 3rd party app/lib/component</extracomment>
+        <translation>%1 está disponible bajo los siguientes términos de licencia:</translation>
+    </message>
+</context>
+</TS>
