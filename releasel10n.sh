ee02da24598b040f7ab579a913c9871cc71bc2de
Buschmann <kontakt@buschmann23.de>
2018-11-10 13:27:09 +0100

    Update translations

--- releasel10n.sh
+++ releasel10n.sh
@@ -2,7 +2,7 @@
 
 lrelease-qt5 -idbased translations/btsc.ts
 
-for LANG in da de es_419 nl sv
+for LANG in da de en_GB en_US es es_419 fr nl sv
 do
 lrelease-qt5 -idbased translations/btsc_$LANG.ts
 done
--- updatel10n.sh
+++ updatel10n.sh
@@ -5,3 +5,5 @@ if [ ! -d translations ]; then
 fi
 
 lupdate-qt5 -no-obsolete -source-language en -target-language en -locations none qml -ts translations/btsc.ts
+lupdate-qt5 -no-obsolete -pluralonly -source-language en -target-language en_GB -locations none qml -ts translations/btsc_en_GB.ts
+lupdate-qt5 -no-obsolete -pluralonly -source-language en -target-language en_US -locations none qml -ts translations/btsc_en_US.ts
