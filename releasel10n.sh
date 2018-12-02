39f21113b32d83136c617217b9e2693ecaf1eaac
Buschmann <mf@huessenbergnetz.de>
2018-12-02 09:04:22 +0100

    Add Chinese translation by rui kon

--- releasel10n.sh
+++ releasel10n.sh
@@ -2,7 +2,7 @@
 
 lrelease-qt5 -idbased translations/hbnsc.ts
 
-for LANG in da de en_GB en_US es es_419 fr nl sv
+for LANG in da de en_GB en_US es es_419 fr nl sv zh
 do
 lrelease-qt5 -idbased translations/hbnsc_$LANG.ts
 done
