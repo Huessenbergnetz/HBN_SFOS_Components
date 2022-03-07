b76b5847698394566897d02253fca10ba7499508
Buschmann <mf@huessenbergnetz.de>
2022-03-07 14:31:14 +0100

    Do not link against libdbus, use macros for dbus version

--- src/hbnsclicensemodel.cpp
+++ src/hbnsclicensemodel.cpp
@@ -189,15 +189,10 @@ LicenseModel::LicenseModel(QObject *parent) : QAbstractListModel(parent)
 #endif
 
 #ifdef HBNSC_WITH_DBUS
-    int dbus_maj_ver = 0;
-    int dbus_min_ver = 0;
-    int dbus_mic_ver = 0;
-    dbus_get_version(&dbus_maj_ver, &dbus_min_ver, &dbus_mic_ver);
-    QVersionNumber dbusVersion(dbus_maj_ver, dbus_min_ver, dbus_mic_ver);
     m_items.emplace_back(
                 QStringLiteral("libdbus-1"),
                 QStringLiteral("D-Bus-Team"),
-                dbusVersion.toString(),
+                QStringLiteral(DBUS_VERSION_STRING),
                 QUrl(QStringLiteral("http://freedesktop.org/wiki/Software/dbus")),
                 QString(),
                 QStringLiteral("GNU General Public License, Version 2"),
